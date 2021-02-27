3ce9ff23343c3f50bbefb9a2b27a3949b75e7fc9
SamuelXXX <samueltung9@gmail.com>
2021-02-27 14:44:58 +0800

    Add support to .lua files

---
+++ Assets/LuaAssetImporter/Editor/LuaAssetImporter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+[ScriptedImporter(1,"lua")]
+public class LuaAssetImporter : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        //Debug.Log("Import Lua Script:" + ctx.assetPath);
+        FileInfo fi = new FileInfo(ctx.assetPath);
+
+        string script = "";
+        using (StreamReader sw = fi.OpenText())
+        {
+            script=sw.ReadToEnd();
+            sw.Close();
+        }
+
+        //Debug.Log("Lua Script Read:" + script);
+        TextAsset luaAsset = new TextAsset(script);
+
+        ctx.AddObjectToAsset("lua asset", luaAsset);
+        ctx.SetMainObject(luaAsset);
+    }
+}
---
+++ Assets/LuaAssetImporter/Editor/LuaAssetImporterEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+[CustomEditor(typeof(LuaAssetImporter))]
+public class LuaAssetImporterEditor : ScriptedImporterEditor
+{
+    protected override bool needsApplyRevert =>false;
+    public override void OnInspectorGUI()
+    {
+    }
+}
