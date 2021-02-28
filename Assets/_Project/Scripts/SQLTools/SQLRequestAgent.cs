c483c060edb091a42262aec42c82d263e072d318
SamuelXXX <samueltung9@gmail.com>
2021-02-28 17:06:00 +0800

    Add demo lua scripts

--- Assets/_Project/Scripts/SQLTools/SQLRequestAgent.cs
+++ Assets/_Project/Scripts/SQLTools/SQLRequestAgent.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using UnityEngine;
 using XLua;
 using System.Data;
--- Assets/_Project/_AssetBundles/_DiscoverContentBundle/LuaScripts/demo.lua
+++ Assets/_Project/_AssetBundles/_DiscoverContentBundle/LuaScripts/demo.lua
@@ -1,11 +1,22 @@
+local util=require 'xlua.util'
+
 function start()
-    print("Start")
+	local query = "select steam_id,user_name from players_basic limit 50";
+    CS.SQLRequestAgent.Instance:PerformSQLRequest(query,callback)
+	self:StartCoroutine(util.cs_generator(query_routine))
 end
 
-function update()
-    print("Update")
+function callback(reader)
+	while(reader:Read()) do
+        local steamId = reader:GetString("steam_id")
+        local userName = reader:GetString("user_name")
+        print("SteamID:"..steamId..",UserName:"..userName)
+	end
 end
 
-function ondestroy()
-    print("OnDestroy")
+function query_routine()
+	while true do
+		print("Test")
+		coroutine.yield(CS.UnityEngine.WaitForSeconds(1))
+	end
 end
